4f5ac9e81f0f692e2c9356ee2244eddb75b4e940
Anessa Arnold <anessaarnold@icloud.com>
2017-12-26 14:44:35 -0800

    game logic and restart button

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-const grid = [
-  [{},{},{},{},{},{},{}],
-  [{},{},{},{},{},{},{}],
-  [{},{},{},{},{},{},{}],
-  [{},{},{},{},{},{},{}],
-  [{},{},{},{color:"red"},{color:"black"},{},{}]
-];
+let grid = [];
+let gameInPlay = false;
+
+window.restart = () => {
+  grid = [
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}]
+  ];
+  gameInPlay = true;
+  document.getElementById("whoWon").innerHTML = " ";
+
+  render();
+}
+
 
 const render = () => {
   const svg = document.getElementById("svg");
@@ -14,14 +24,85 @@ const render = () => {
     for (var j = 0; j < row.length; j++) {
       const square = grid[i][j];
       const color = square && square.color || 'gray';
-      doc = doc +  `<circle onClick="clickSquare(${i}, ${j})" fill='${color}' r='30px' cx='${j * 70 + 50}px' cy='${i * 70 + 50}px'></circle>` 
+      doc = doc +  `<circle onClick="clickSquare(${j}, ${i})" fill='${color}' r='30px' cx='${j * 70 + 50}px' cy='${i * 70 + 50}px'></circle>` 
     }
   }
   svg.innerHTML = doc;
 };
 
+let currentColor = "red";
+
 window.clickSquare = (x, y) => {
-  console.log("you clicked square", x,y);
+  if (!gameInPlay) {
+    return;
+  }
+  for (var i = grid.length - 1; i >= 0; i--) {
+    var row = grid[i];
+    var targetPlace = row[x];
+    if (!targetPlace.color) {
+      row[x] = {color: currentColor};
+      currentColor = currentColor === "red" ? "black" : "red";
+      render();
+      calculatewinners();
+      return;
+    }
+  }
 }
 
-render();
+function calculatewinners() {
+  console.log("is there a winner");
+  // find verticals
+  for (var i = 0; i < grid.length; i++) {
+    var row = grid[i];
+    for (var j = 0; j < row.length; j++) {
+      var square = grid[i][j];
+      if (square && square.color) {
+        if (i === 0 || i === 1) {
+          if (grid[i + 1][j].color === square.color && 
+            grid[i + 2][j].color === square.color && 
+            grid[i + 3][j].color === square.color) {
+            colorWins(square.color);
+            return;
+            
+            }
+          }
+          if (j === 0 || j === 1 || j === 2 || j === 3) {
+            if (grid[i][j + 1].color === square.color  &&
+              grid[i][j + 2].color === square.color &&
+              grid[i][j + 3].color === square.color) {
+              colorWins(square.color);                
+              return;
+              }
+            }
+
+          if (i === 0 || i === 1) {
+            if (j === 0 || j === 1 || j === 2 || j === 3) {
+              if (grid[i + 1][j + 1].color === square.color &&
+                  grid[i + 2][j + 2].color === square.color &&
+                  grid[i + 3][j + 3].color === square.color) {
+                  colorWins(square.color);
+                  return;
+                  }
+            }
+
+            if (j === 3 || j === 4 || j === 5 || j === 6) {
+              if (grid[i + 1][j - 1].color === square.color &&
+                  grid[i + 2][j - 2].color === square.color &&
+                  grid[i + 3][j - 3].color === square.color) {
+                  colorWins(square.color);
+                  return;
+                  }
+            }
+          }
+          }
+        }
+        
+      }
+    
+    function colorWins(color) {
+      document.getElementById("whoWon").innerHTML = `${color} wins!`;
+    }
+    
+  }
+
+restart();
