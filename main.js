2421f98f43202c950c78b0f9b163771956b5ff8b
Anessa Arnold <anessaarnold@icloud.com>
2017-12-26 13:18:17 -0800

    inital commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const grid = [
+  [{},{},{},{},{},{},{},{},],
+  [{},{},{},{},{},{},{},{},],
+  [{},{},{},{},{},{},{},{},],
+  [{},{},{},{},{},{},{},{},],
+  [{},{},{},{},{color:"red"},{},{},{},]
+];
+
+const render = () => {
+  const svg = document.getElementById("svg");
+  const doc = "";
+  for (var i = 0; i < grid.length; i++) {
+    var row = grid[i];
+    for (var j = 0; j < row.length; j++) {
+      doc = doc +  `<circle fill="black" r="1" x=${j}px y=${i}px` 
+    }
+  };
+};
