8c8b1a1b022c036353af524a2c27d448cb3e7401
Anessa Arnold <anessaarnold@icloud.com>
2017-12-26 13:35:54 -0800

    board game

--- main.js
+++ main.js
@@ -3,16 +3,21 @@ const grid = [
   [{},{},{},{},{},{},{},{},],
   [{},{},{},{},{},{},{},{},],
   [{},{},{},{},{},{},{},{},],
-  [{},{},{},{},{color:"red"},{},{},{},]
+  [{},{},{},{},{color:"red"},{color:"black"},{},{},]
 ];
 
 const render = () => {
   const svg = document.getElementById("svg");
-  const doc = "";
+  let doc = ``;
   for (var i = 0; i < grid.length; i++) {
     var row = grid[i];
     for (var j = 0; j < row.length; j++) {
-      doc = doc +  `<circle fill="black" r="1" x=${j}px y=${i}px` 
+      const square = grid[i][j];
+      const color = square && square.color || 'gray';
+      doc = doc +  `<circle fill='${color}' r='30px' cx='${j * 70 + 50}px' cy='${i * 70 + 50}px'></circle>` 
     }
-  };
+  }
+  svg.innerHTML = doc;
 };
+
+render();
