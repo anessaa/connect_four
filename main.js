ee2792c30592dc706df5606736bf8dab2894bf86
Anessa Arnold <anessaarnold@icloud.com>
2017-12-26 13:47:01 -0800

    game board refactor

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const grid = [
-  [{},{},{},{},{},{},{},{},],
-  [{},{},{},{},{},{},{},{},],
-  [{},{},{},{},{},{},{},{},],
-  [{},{},{},{},{},{},{},{},],
-  [{},{},{},{},{color:"red"},{color:"black"},{},{},]
+  [{},{},{},{},{},{},{}],
+  [{},{},{},{},{},{},{}],
+  [{},{},{},{},{},{},{}],
+  [{},{},{},{},{},{},{}],
+  [{},{},{},{color:"red"},{color:"black"},{},{}]
 ];
 
 const render = () => {
@@ -14,10 +14,14 @@ const render = () => {
     for (var j = 0; j < row.length; j++) {
       const square = grid[i][j];
       const color = square && square.color || 'gray';
-      doc = doc +  `<circle fill='${color}' r='30px' cx='${j * 70 + 50}px' cy='${i * 70 + 50}px'></circle>` 
+      doc = doc +  `<circle onClick="clickSquare(${i}, ${j})" fill='${color}' r='30px' cx='${j * 70 + 50}px' cy='${i * 70 + 50}px'></circle>` 
     }
   }
   svg.innerHTML = doc;
 };
+
+window.clickSquare = (x, y) => {
+  console.log("you clicked square", x,y);
+}
 
 render();
